7b29c087d4da10e294857a5ddcb2d5aceb81aee7
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-05-06 07:36:07 +0200

    CFE-3396: promise module for systemd
    Changelog: none
    Ticket: CFE-3396
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

---
+++ promise_types/systemd/README.md
@@ -0,0 +1,114 @@
+# systemd promise module
+
+## Synopsis
+
+* *Name*: `systemd`
+* *Version*: `0.1.0`
+* *Description*: Create and manage services using systemd
+
+## Requirements
+
+* A system managed by systemd.
+
+## Attributes
+
+| Name | Type | Description| Mandatory | Default |
+| --- | --- | --- | --- | --- |
+| `daemon_reexec` | `boolean` | Run daemon-reexec before performing actions to the service | No | `false` |
+| `daemon_reload` | `boolean` | Run daemon-reload before performing actions to the service | No | `false` |
+| `enabled` | `boolean` | Start the service on boot | No | `true` |
+| `masked` | `boolean` | Mask the service, making it impossible to start | No | `false` |
+| `name` | `string` | The name of the service | Yes | Promiser |
+| `replace` | `boolean` | Replace service unit file if it already exists | No | `True` |
+| `state` | `string` | State of the service: `started`, `stopped` and `absent` are idempotent, `restart` and `reload` are not. | Yes | - |
+| `unit_description` | `string` | Free-form string describing the unit | No | - |
+| `unit_requires` | `slist` | Units which must be started along with the service | No | - |
+| `unit_wants` | `slist` | Units which should be started along with the service | No | - |
+| `unit_after` | `slist` | Units which must be started after the service | No | - |
+| `unit_before` | `slist` | Units which must be started before the service | No | - |
+| `unit_extra` | `slist` | Additional lines to append to the `Unit` section of the service file | No | - |
+| `service_type` | `string` | Process start-up type, e.g. `simple`, `forking`, etc. | No | - |
+| `service_pid_file` | `string` | Absolute file name pointing to the PID file of this daemon | No | - |
+| `service_user` | `string` | User under which the service should be executed | No | - |
+| `service_group` | `string` | Group under which the service should be executed | No | - |
+| `service_nice` | `integer` | Default nice level for the service | No | - |
+| `service_oom_score_adjust` | `integer` | Adjustment level for the Out-Of-Memory killer | No | - |
+| `service_exec_start` | `slist` | Commands that are executed when this service is started | No | - |
+| `service_exec_start_pre` | `slist` | Commands that are executed before `exec_start` commands | No | - |
+| `service_exec_start_post` | `slist` | Commands that are executed after `exec_start` commands | No | - |
+| `service_exec_stop` | `slist` | Commands to execute to stop the service started via `exec_start` | No | - |
+| `service_exec_stop_post` | `slist` | Commands that are executed after the service is stopped | No | - |
+| `service_exec_reload` | `slist` | Commands to execute to trigger a configuration reload in the service | No | - |
+| `service_restart` | `string` | When the service must be restarted | No | - |
+| `service_restart_sec` | `string` | Time to sleep before restarting the service | No | - |
+| `service_timeout_sec` | `string` | Maximum waiting time for start/stop commands processing | No | - |
+| `service_environment` | `slist` | Environment variables, in the format `key=value` | No | - |
+| `service_environment_file` | `string` | Path to a file containing the environment variables | No | - |
+| `service_working_directory` | `string` | Working directory | No | - |
+| `service_standard_input` | `string` | Path to the standard input file descriptor | No | - |
+| `service_standard_output` | `string` | Path to the standard output file descriptor | No | - |
+| `service_standard_error` | `string` | Path to the standard error file descriptor | No | - |
+| `service_tty_path` | `string` | Path to the tty if required by the `standard_*` options | No | - |
+| `service_extra` | `slist` | Additional lines to append to the `Service` section of the service file | No | - |
+| `install_wanted_by` | `slist` | Units which are wanted by the service | No | - |
+| `install_required_by` | `slist` | Units which are required by the service | No | - |
+| `install_extra` | `slist` | Additional lines to append to the `Install` section of the service file | No | - |
+
+## Examples
+
+Make sure service named `myservice` is started:
+
+```cfengine3
+bundle agent main
+{
+  systemd:
+    "myservice"
+      name => "myservice",
+      enabled => "true",
+      state => "started";
+}
+```
+
+Make sure a service `sample` running `sleep 86400` exists:
+
+```cfengine3
+bundle agent main
+{
+  systemd:
+    "sample"
+      masked => "false",
+      enabled => "true",
+      state => "started",
+      unit_description => "My sample service",
+      service_exec_start => {"/usr/bin/sleep 86400"},
+      service_type => "simple",
+      install_wanted_by => {"multi-user.target"};
+}
+```
+
+Make sure the service named `sample` does not exist:
+
+```cfengine3
+bundle agent main
+{
+  systemd:
+    "sample"
+      state => "absent";
+}
+```
+
+## Authors
+
+This software was created by the team at [Northern.tech AS](https://northern.tech), with many contributions from the community. Thanks everyone!
+
+[CFengine](https://cfengine.com) is sponsored by [Northern.tech AS](https://northern.tech)
+
+## Contribute
+
+Feel free to open pull requests to expand this documentation, add features or fix problems.
+You can also pick up an existing task or file an issue in [our bug tracker](https://tracker.mender.io/issues/).
+
+## License
+
+This software is licensed under the MIT License. See LICENSE in the root of the repository for the full license text.
+
---
+++ promise_types/systemd/systemd.py
@@ -0,0 +1,394 @@
+import json
+import os
+import subprocess
+
+from enum import Enum
+from typing import Dict, List, Optional, Tuple
+
+from cfengine import PromiseModule, ValidationError, Result
+from pydantic import (
+    BaseModel,
+    ValidationError as PydanticValidationError,
+    validator,
+)
+
+
+SYSTEMD_LIB_PATH = "/lib/systemd/system"
+
+
+class SystemdPromiseTypeStates(Enum):
+    RELOADED = "reloaded"
+    RESTARTED = "restarted"
+    STARTED = "started"
+    STOPPED = "stopped"
+    ABSENT = "absent"
+
+
+class SystemdPromiseTypeModel(BaseModel):
+    daemon_reexec: bool = False
+    daemon_reload: bool = False
+    enabled: bool = True
+    masked: bool = False
+    name: str
+    replace: bool = True
+    state: str
+    unit_description: Optional[str]
+    unit_requires: List[str] = []
+    unit_wants: List[str] = []
+    unit_after: List[str] = []
+    unit_before: List[str] = []
+    unit_extra: List[str] = []
+    service_type: Optional[str]
+    service_pid_file: Optional[str]
+    service_user: Optional[str]
+    service_group: Optional[str]
+    service_nice: Optional[int]
+    service_oom_score_adjust: Optional[int]
+    service_exec_start: List[str] = []
+    service_exec_start_pre: List[str] = []
+    service_exec_start_post: List[str] = []
+    service_exec_stop: List[str] = []
+    service_exec_stop_post: List[str] = []
+    service_exec_reload: List[str] = []
+    service_restart: Optional[str]
+    service_restart_sec: Optional[str]
+    service_timeout_sec: Optional[str]
+    service_environment: List[str] = []
+    service_environment_file: Optional[str]
+    service_working_directory: Optional[str]
+    service_standard_input: Optional[str]
+    service_standard_output: Optional[str]
+    service_standard_error: Optional[str]
+    service_tty_path: Optional[str]
+    service_extra: List[str] = []
+    install_wanted_by: List[str] = []
+    install_required_by: List[str] = []
+    install_extra: List[str] = []
+
+    @validator("state")
+    def state_must_be_valid(cls, v):
+        if v not in (
+            SystemdPromiseTypeStates.RELOADED.value,
+            SystemdPromiseTypeStates.RESTARTED.value,
+            SystemdPromiseTypeStates.STARTED.value,
+            SystemdPromiseTypeStates.STOPPED.value,
+            SystemdPromiseTypeStates.ABSENT.value,
+        ):
+            raise ValueError("invalid value")
+        return v
+
+
+class SystemdPromiseTypeModule(PromiseModule):
+    def __init__(self, **kwargs):
+        super(SystemdPromiseTypeModule, self).__init__(
+            "systemd_promise_module", "0.1.0", **kwargs
+        )
+
+    def validate_promise(self, promiser: str, attributes: Dict):
+        attributes.setdefault("name", promiser)
+        if type(attributes.get("environment")) == str:
+            attributes["environment"] = json.loads(attributes["environment"])
+        try:
+            SystemdPromiseTypeModel(**attributes)
+        except PydanticValidationError as e:
+            errors = [
+                ".".join(map(str, err["loc"])) + ": " + err["msg"] for err in e.errors()
+            ]
+            raise ValidationError(", ".join(errors))
+
+    def evaluate_promise(
+        self, promiser: str, attributes: Dict
+    ) -> Tuple[str, List[str]]:
+        safe_promiser = promiser.replace(",", "_")
+        attributes.setdefault("name", promiser)
+        if type(attributes.get("environment")) == str:
+            attributes["environment"] = json.loads(attributes["environment"])
+        model = SystemdPromiseTypeModel(**attributes)
+        # get the status of the service
+        try:
+            output = self._exec_command(
+                [
+                    "systemctl",
+                    "show",
+                    model.name,
+                    "-p",
+                    "ActiveState",
+                    "-p",
+                    "SubState",
+                    "-p",
+                    "UnitFileState",
+                ]
+            )
+            service_status = dict(k.split("=", 1) for k in output.strip().splitlines())
+        except subprocess.CalledProcessError as e:
+            self.log_error(f"Failed to run systemctl: {e.output or e}")
+            e.stderr and self.log_error(e.stderr.strip())
+            return (Result.NOT_KEPT, [f"{safe_promiser}_show_failed"])
+        # apply the changes
+        if model.state == SystemdPromiseTypeStates.ABSENT.value:
+            return self._service_absent(model, safe_promiser, service_status)
+        else:
+            return self._service_present(model, safe_promiser, service_status)
+
+    def _service_absent(
+        self, model: SystemdPromiseTypeModel, safe_promiser: str, service_status: dict
+    ) -> Tuple[str, List[str]]:
+        classes = []
+        result = Result.KEPT
+        # stop the service, in case it is running
+        if (
+            service_status["ActiveState"] == "active"
+            and service_status["SubState"] == "running"
+        ):
+            try:
+                self._exec_command(["systemctl", "stop", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_daemon_reexec_failed"])
+            self.log_info(f"Stopped the service {model.name}")
+        # disable the service, in case it is enabled
+        if service_status["ActiveState"] == "active":
+            try:
+                self._exec_command(["systemctl", "disable", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_daemon_reexec_failed"])
+            self.log_info(f"Disabled the service {model.name}")
+        # remove the service file
+        path = os.path.join(SYSTEMD_LIB_PATH, f"{model.name}.service")
+        if os.path.exists(path):
+            try:
+                os.unlink(path)
+                result = Result.REPAIRED
+            except OSError as e:
+                self.log_error(f"Failed to remove the service file: {e}")
+                return (Result.NOT_KEPT, [f"{safe_promiser}_remove_failed"])
+            self.log_info(f"Removed the service {model.name}")
+        # reload systemctl, if needed
+        if result == Result.REPAIRED:
+            try:
+                self._exec_command(["systemctl", "daemon-reload"])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_remove_failed"])
+            self.log_info(f"Reloaded the list of services")
+            classes.append(f"{safe_promiser}_absent")
+        return (result, classes)
+
+    def _service_present(
+        self, model: SystemdPromiseTypeModel, safe_promiser: str, service_status: dict
+    ) -> Tuple[str, List[str]]:
+        classes = []
+        result = Result.KEPT
+        # render the template of the service
+        service_template = self._render_service_template(model)
+        # create the service file if it doesn't exist, or replace it if the content
+        # doesn't match our template and replace is true
+        service_path = os.path.join(SYSTEMD_LIB_PATH, f"{model.name}.service")
+        try:
+            if (
+                not os.path.exists(service_path)
+                or model.replace
+                and open(service_path).read() != service_template
+            ):
+                open(service_path, "w").write(service_template)
+                result = Result.REPAIRED
+                self.log_info(f"Installed the service {model.name}")
+                classes.append(f"{safe_promiser}_installed")
+        except OSError as e:
+            self.log_error(f"Failed to install the service file: {e}")
+            return (Result.NOT_KEPT, [f"{safe_promiser}_install_failed"])
+        # run systemctl daemon-reexec
+        if model.daemon_reexec:
+            try:
+                self._exec_command(["systemctl", "daemon-reexec"])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_daemon_reexec_failed"])
+            self.log_info("Executed systemctl daemon-reexec")
+            classes.append(f"{safe_promiser}_daemon_reexec")
+        # run systemctl daemon-reload
+        elif result == Result.REPAIRED or model.daemon_reload:
+            try:
+                self._exec_command(["systemctl", "daemon-reload"])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_daemon_reload_failed"])
+            self.log_info("Executed systemctl daemon-reload")
+            classes.append(f"{safe_promiser}_daemon_reload")
+        # mask the service
+        if model.masked and service_status["UnitFileState"] != "masked":
+            try:
+                self._exec_command(["systemctl", "mask", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_mask_failed"])
+            self.log_info(f"Masked the service {model.name}")
+            classes.append(f"{safe_promiser}_masked")
+        # unmask the service
+        elif not model.masked and service_status["UnitFileState"] == "masked":
+            try:
+                self._exec_command(["systemctl", "unmask", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_mask_failed"])
+            self.log_info(f"Unmasked the service {model.name}")
+            classes.append(f"{safe_promiser}_unmasked")
+        # enable the service
+        if (
+            model.enabled
+            and not model.masked
+            and service_status["UnitFileState"] != "enabled"
+        ):
+            try:
+                self._exec_command(["systemctl", "enable", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_enable_failed"])
+            self.log_info(f"Enabled the service {model.name}")
+            classes.append(f"{safe_promiser}_enabled")
+        # disable the service
+        elif (
+            not model.enabled
+            and not model.masked
+            and service_status["UnitFileState"] == "enabled"
+        ):
+            try:
+                self._exec_command(["systemctl", "disable", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_disable_failed"])
+            self.log_info(f"Disabled the service {model.name}")
+            classes.append(f"{safe_promiser}_disabled")
+        # start the service, if not running
+        if model.state == SystemdPromiseTypeStates.STARTED.value and not (
+            service_status["ActiveState"] == "active"
+            and service_status["SubState"] == "running"
+        ):
+            try:
+                self._exec_command(["systemctl", "start", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_start_failed"])
+            self.log_info(f"Started the service {model.name}")
+            classes.append(f"{safe_promiser}_started")
+        # stop the service, if running
+        elif (
+            model.state == SystemdPromiseTypeStates.STOPPED.value
+            and service_status["ActiveState"] == "active"
+            and service_status["SubState"] == "running"
+        ):
+            try:
+                self._exec_command(["systemctl", "stop", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_stop_failed"])
+            self.log_info(f"Stopped the service {model.name}")
+            classes.append(f"{safe_promiser}_stopped")
+        # reload the service
+        elif model.state == SystemdPromiseTypeStates.RELOADED.value:
+            try:
+                self._exec_command(["systemctl", "reload", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_reload_failed"])
+            self.log_info(f"Reloaded the service {model.name}")
+            classes.append(f"{safe_promiser}_reloaded")
+        # restart the service
+        elif model.state == SystemdPromiseTypeStates.RESTARTED.value:
+            try:
+                self._exec_command(["systemctl", "restart", model.name])
+            except subprocess.CalledProcessError as e:
+                self.log_error(f"Failed to run systemctl: {e.output or e}")
+                e.stderr and self.log_error(e.stderr.strip())
+                return (Result.NOT_KEPT, [f"{safe_promiser}_restart_failed"])
+            self.log_info(f"Restarted the service {model.name}")
+            classes.append(f"{safe_promiser}_restarted")
+        return (result, classes)
+
+    def _exec_command(self, args: List[str], cwd: Optional[str] = None) -> str:
+        self.log_verbose(f"Run: {' '.join(args)}")
+        output = subprocess.check_output(
+            args, cwd=cwd, stderr=subprocess.PIPE, text=True,
+        ).strip()
+        output != "" and self.log_verbose(output)
+        return output
+
+    def _render_service_template(self, model: SystemdPromiseTypeModel) -> str:
+        blocks = {
+            "unit": [],
+            "service": [],
+            "install": [],
+        }
+        for block, attr, key in (
+            ("unit", "unit_description", "Description"),
+            ("unit", "unit_requires", "Requires"),
+            ("unit", "unit_wants", "Wants"),
+            ("service", "service_type", "Type"),
+            ("service", "service_pid_file", "PIDFile"),
+            ("service", "service_user", "User"),
+            ("service", "service_group", "Group"),
+            ("service", "service_nice", "Nice"),
+            ("service", "service_oom_score_adjust", "OOMScoreAdjust"),
+            ("service", "service_exec_start", "ExecStart"),
+            ("service", "service_exec_start_pre", "ExecStartPre"),
+            ("service", "service_exec_start_post", "ExecStartPost"),
+            ("service", "service_exec_stop", "ExecStop"),
+            ("service", "service_exec_stop_post", "ExecStopPost"),
+            ("service", "service_exec_reload", "ExecReload"),
+            ("service", "service_restart", "Restart"),
+            ("service", "service_restart_sec", "RestartSec"),
+            ("service", "service_timeout_sec", "TimeoutStartSec"),
+            ("service", "service_environment", "Environment"),
+            ("service", "service_environment_file", "EnvironmentFile"),
+            ("service", "service_working_directory", "WorkingDirectory"),
+            ("service", "service_standard_input", "StandardInput"),
+            ("service", "service_standard_output", "StandardOutput"),
+            ("service", "service_standard_error", "StandardError"),
+            ("service", "service_tty_path", "TTYPath"),
+            ("install", "install_wanted_by", "WantedBy"),
+            ("install", "install_required_by", "RequiredBy"),
+        ):
+            value = getattr(model, attr)
+            if value is None:
+                continue
+            elif type(value) == list:
+                for item in value:
+                    blocks[block].append(f"{key}={item}")
+            else:
+                blocks[block].append(f"{key}={str(value)}")
+        #
+        if model.unit_extra:
+            blocks["unit"].extend(model.unit_extra)
+        unit_section = "\n".join(blocks["unit"])
+        #
+        if model.service_extra:
+            blocks["unit"].extend(model.service_extra)
+        service_section = "\n".join(blocks["service"])
+        #
+        if model.install_extra:
+            blocks["unit"].extend(model.install_extra)
+        install_section = "\n".join(blocks["install"])
+        #
+        return (
+            f"# Rendered by CFEngine. Do not edit directly\n\n"
+            + f"[Unit]\n{unit_section}\n\n"
+            + f"[Service]\n{service_section}\n\n"
+            + f"[Install]\n{install_section}\n"
+        )
+
+
+if __name__ == "__main__":
+    SystemdPromiseTypeModule().start()
