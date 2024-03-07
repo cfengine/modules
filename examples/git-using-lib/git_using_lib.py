10101ee997cc2dbc3678a846d6c67329e588cc6f
Ole Herman Schumacher Elgesem <4048546+olehermanse@users.noreply.github.com>
2024-03-07 14:51:51 +0100

    Updated git_using_lib.py example to match blog post
    See:
    https://github.com/cfengine/website/pull/972

--- examples/git-using-lib/git_using_lib.py
+++ examples/git-using-lib/git_using_lib.py
@@ -3,38 +3,33 @@ from cfengine import PromiseModule, ValidationError, Result
 
 
 class GitPromiseTypeModule(PromiseModule):
-    def __init__(self, **kwargs):
-        super().__init__("git_promise_module", "0.0.3", **kwargs)
+    def __init__(self):
+        super().__init__("git_promise_module", "0.0.1")
 
     def validate_promise(self, promiser, attributes, metadata):
         if not promiser.startswith("/"):
             raise ValidationError(f"File path '{promiser}' must be absolute")
-        for name, value in attributes.items():
-            if name != "repo":
-                raise ValidationError(f"Unknown attribute '{name}' for git promises")
-            if name == "repo" and type(value) is not str:
-                raise ValidationError(f"'repo' must be string for git promise types")
+        if "repository" not in attributes:
+            raise ValidationError(f"Attribute 'repository' is required")
 
     def evaluate_promise(self, promiser, attributes, metadata):
+        url = attributes["repository"]
         folder = promiser
-        url = attributes["repo"]
-
-        safe_promiser = promiser.replace(",", "_")
 
         if os.path.exists(folder):
-            return (Result.KEPT, [f"{safe_promiser}_cloned_already"])
+            self.log_verbose(f"'{folder}' already exists, nothing to do")
+            return Result.KEPT
 
         self.log_info(f"Cloning '{url}' -> '{folder}'...")
         os.system(f"git clone {url} {folder} 2>/dev/null")
 
         if os.path.exists(folder):
             self.log_info(f"Successfully cloned '{url}' -> '{folder}'")
-            return (Result.REPAIRED, [f"{safe_promiser}_cloned"])
+            return Result.REPAIRED
         else:
             self.log_error(f"Failed to clone '{url}' -> '{folder}'")
-            return (Result.NOT_KEPT, [f"{safe_promiser}_clone_failed"])
+            return Result.NOT_KEPT
 
 
 if __name__ == "__main__":
-    # GitPromiseTypeModule(record_file_path="/tmp/git_using_lib.log").start()
     GitPromiseTypeModule().start()
